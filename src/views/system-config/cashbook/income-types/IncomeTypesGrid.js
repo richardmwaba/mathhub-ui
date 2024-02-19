9c70ceb5809eaafa71a3b43156cffce8f0801e51
Richard Mwaba <ricch.mwaba@gmail.com>
2024-02-18 22:14:13 -0600

    Added logic to create and edit income types for cashbook configuration. (#13)
    Co-authored-by: Richard <richard.w.mwaba@gmail.com>

--- src/views/system-config/cashbook/income-types/IncomeTypesGrid.js
+++ src/views/system-config/cashbook/income-types/IncomeTypesGrid.js
@@ -1,41 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import {
-    CButton,
-    CCardBody,
-    CCollapse,
-    CFormCheck,
-    CFormLabel,
-    CSmartTable,
-} from '@coreui/react-pro';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, useRef } from 'react';
+import { CButton, CCardBody, CSmartTable, CToaster } from '@coreui/react-pro';
 import useAxiosPrivate from 'src/hooks/useAxiosPrivate.js';
 import IncomeTypesService from 'src/api/system-config/cashbook/income-types.service';
+import CIcon from '@coreui/icons-react';
+import NewIncomeTypeForm from './NewIncomeTypeForm';
+import { SuccessToast } from 'src/components/common/SuccessToast';
+import EditIncomeTypeForm from './EditIncomeTypeForm';
+import { EditButton } from 'src/components/common/EditButton';
+import { cilPlus } from '@coreui/icons';
 
 export default function IncomeTypesGrid() {
     const axiosPrivate = useAxiosPrivate();
+    const controller = new AbortController();
 
-    const [selected, setSelected] = useState([]);
-    const [details, setDetails] = useState([]);
     const [incomeTypes, setIncomeTypes] = useState([]);
+    const [createdIncomeType, setCreatedIncomeType] = useState({});
     const [error, setError] = useState('');
+    const [isMounted, setIsMounted] = useState(true);
+    const [isVisibleEditIncomeTypeModal, setIsVisibleEditIncomeTypeModal] = useState(false);
+    const [isVisibleNewIncomeTypeModal, setIsVisibleNewIncomeTypeModal] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [savedIncomeType, setSavedIncomeType] = useState({});
+    const [selectedIncomeType, setSelectedIncomeType] = useState({});
+    const [toast, setToast] = useState(0);
 
-    const incomeTypesWithSelect = incomeTypes?.map((incomeType) => {
-        const _selected = selected.includes(incomeType.id);
-        return {
-            ...incomeType,
-            incomeType,
-            _selected,
-            _classes: [incomeType._classes, _selected && 'table-selected'],
-        };
-    });
+    const incomeTypeActionSuccessToasterRef = useRef();
 
     const columns = [
-        { key: 'select', label: '', filter: false, sorter: false },
+        { key: 'name', label: 'Name', _style: { width: '40%' } },
         {
             key: 'description',
-            label: 'Name',
+            label: 'Description',
             _style: { width: '60%' },
         },
-        { key: 'type', label: 'Short Name', _style: { width: '40%' } },
         {
             key: 'show_details',
             label: '',
@@ -45,123 +43,122 @@ export default function IncomeTypesGrid() {
         },
     ];
 
-    const toggleDetails = (index) => {
-        const position = details.indexOf(index);
-        let newDetails = details.slice();
-        if (position !== -1) {
-            newDetails.splice(position, 1);
-        } else {
-            newDetails = [...details, index];
-        }
-        setDetails(newDetails);
+    const getIncomeTypes = async () => {
+        const response = await IncomeTypesService.getAllIncomeTypes(
+            axiosPrivate,
+            controller,
+            setError,
+        );
+        isMounted && setIncomeTypes(response);
+        setLoading(false);
     };
 
-    const check = (e, id) => {
-        if (e.target.checked) {
-            setSelected([...selected, id]);
-        } else {
-            setSelected(selected.filter((itemId) => itemId !== id));
-        }
+    const setCreatedIncomeTypeAndRefreshIncomeTypes = (newIncomeType) => {
+        setCreatedIncomeType(newIncomeType);
+        getIncomeTypes();
     };
 
-    // get students data from api
+    const setSavedIncomeTypeAndRefreshIncomeTypes = (savedEditedIncomeType) => {
+        setSavedIncomeType(savedEditedIncomeType);
+        getIncomeTypes();
+    };
+
+    // get income types data from api
     useEffect(() => {
-        let isMounted = true;
-        const controller = new AbortController();
-
-        const getPaymentMethods = async () => {
-            const response = await IncomeTypesService.getAllIncomeTypes(
-                axiosPrivate,
-                controller,
-                setError,
-            );
-            isMounted && setIncomeTypes(response);
-        };
-
-        getPaymentMethods();
+        getIncomeTypes();
 
         return () => {
-            isMounted = false;
+            setIsMounted(false);
             controller.abort();
         };
-    }, [axiosPrivate]);
+    }, []);
 
-    return (
-        <CCardBody>
-            <CSmartTable
-                sorterValue={{ column: 'description', state: 'asc' }}
-                items={incomeTypesWithSelect}
-                columns={columns}
-                itemsPerPage={10}
-                columnFilter
-                columnSorter
-                tableFilter
-                cleaner
-                itemsPerPageSelect
-                pagination
-                scopedColumns={{
-                    select: (item) => {
-                        return (
-                            <td>
-                                <CFormCheck
-                                    id={`checkbox${item.id}`}
-                                    checked={item._selected}
-                                    onChange={(e) => check(e, item.id)}
-                                />
-                                <CFormLabel
-                                    variant="custom-checkbox"
-                                    htmlFor={`checkbox${item.id}`}
-                                />
-                            </td>
-                        );
-                    },
-                    show_details: (item) => {
-                        return ToggleButton(toggleDetails, item, details);
-                    },
-                    details: (item) => {
-                        return DetailsCard(details, item);
-                    },
-                }}
-                tableProps={{
-                    hover: true,
-                    responsive: true,
-                }}
+    useEffect(() => {
+        const incomeTypeSuccessfullyCreatedToast = (
+            <SuccessToast
+                message={`Income type ${createdIncomeType?.typeName} has been created successfully`}
             />
-        </CCardBody>
-    );
-}
+        );
 
-function DetailsCard(details, item) {
+        if (createdIncomeType?.typeName) {
+            setToast(incomeTypeSuccessfullyCreatedToast);
+        }
+    }, [createdIncomeType]);
+
+    useEffect(() => {
+        const incomeTypeSuccessfullyEditedToast = (
+            <SuccessToast
+                message={`Income type ${savedIncomeType?.typeName} has been updated successfully`}
+            />
+        );
+
+        if (savedIncomeType?.typeName) {
+            setToast(incomeTypeSuccessfullyEditedToast);
+        }
+    }, [savedIncomeType]);
+
     return (
-        <CCollapse visible={details.includes(item.id)}>
+        <>
             <CCardBody>
-                <h4>{item.username}</h4>
-                <p className="text-muted">User since: {item.registered}</p>
-                <CButton size="sm" color="info">
-                    User Settings
+                <CButton
+                    color="primary"
+                    className="mb-2"
+                    variant="outline"
+                    size="sm"
+                    onClick={() => setIsVisibleNewIncomeTypeModal(!isVisibleNewIncomeTypeModal)}
+                >
+                    <CIcon icon={cilPlus} title="Add New Income Type" /> Add New Income Type
                 </CButton>
-                <CButton size="sm" color="danger" className="ml-1">
-                    Delete
-                </CButton>
+                <CSmartTable
+                    sorterValue={{ column: 'description', state: 'asc' }}
+                    items={incomeTypes}
+                    columns={columns}
+                    itemsPerPage={10}
+                    columnFilter
+                    columnSorter
+                    tableFilter
+                    loading={loading}
+                    cleaner
+                    itemsPerPageSelect
+                    pagination
+                    noItemsLabel={
+                        error
+                            ? `Could not retrieve income types due to ${error}. Please try again.`
+                            : 'No income types found'
+                    }
+                    scopedColumns={{
+                        show_details: (item) => (
+                            <EditButton
+                                item={item}
+                                setSelectedItem={setSelectedIncomeType}
+                                isVisibleEditModal={isVisibleEditIncomeTypeModal}
+                                setIsVisibleEditModal={setIsVisibleEditIncomeTypeModal}
+                            />
+                        ),
+                    }}
+                    tableProps={{
+                        hover: true,
+                        responsive: true,
+                        striped: true,
+                    }}
+                />
             </CCardBody>
-        </CCollapse>
-    );
-}
-
-function ToggleButton(toggleDetails, item, details) {
-    return (
-        <td className="py-2">
-            <CButton
-                color="primary"
-                variant="outline"
-                shape="square"
-                size="sm"
-                onClick={() => {
-                    toggleDetails(item.id);
-                }}
-            >
-                {details.includes(item.id) ? 'Hide' : 'Show'}
-            </CButton>
-        </td>
+            {isVisibleNewIncomeTypeModal && (
+                <NewIncomeTypeForm
+                    visibility={isVisibleNewIncomeTypeModal}
+                    setIncomeTypeModalVisibility={setIsVisibleNewIncomeTypeModal}
+                    createdIncomeTypeCallBack={setCreatedIncomeTypeAndRefreshIncomeTypes}
+                />
+            )}
+            {isVisibleEditIncomeTypeModal && (
+                <EditIncomeTypeForm
+                    incomeType={selectedIncomeType}
+                    visibility={isVisibleEditIncomeTypeModal}
+                    setEditIncomeTypeModalVisibility={setIsVisibleEditIncomeTypeModal}
+                    savedIncomeTypeCallBack={setSavedIncomeTypeAndRefreshIncomeTypes}
+                />
+            )}
+            <CToaster ref={incomeTypeActionSuccessToasterRef} push={toast} placement="bottom-end" />
+        </>
     );
 }
