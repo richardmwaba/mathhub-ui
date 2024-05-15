0a36a55b37103c272ce82c5751c985bd5b14cdf1
Richard <ricch.mwaba@gmail.com>
2024-05-14 19:58:11 -0500

    Add logic to create, edit and view expenses so that users can record expenses.

---
+++ src/views/cashbook/expenses/EditExpenseForm.js
@@ -0,0 +1,284 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState } from 'react';
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCol,
+    CContainer,
+    CForm,
+    CFormInput,
+    CModal,
+    CModalBody,
+    CModalFooter,
+    CModalHeader,
+    CModalTitle,
+    CRow,
+    CLoadingButton,
+    CFormText,
+    CFormSelect,
+    CFormLabel,
+    CInputGroup,
+    CInputGroupText,
+} from '@coreui/react-pro';
+import useAxiosPrivate from 'src/hooks/useAxiosPrivate.js';
+import PropTypes from 'prop-types';
+import ExpensesService from 'src/api/cashbook/expenses.service';
+import { CFormInputWithMask } from 'src/views/common/CFormInputWithMask';
+import ExpenseTypesService from 'src/api/system-config/cashbook/expense-types.service';
+import PaymentMethodsService from 'src/api/system-config/cashbook/payment-methods.service';
+
+export default function EditExpenseForm({
+    expense,
+    visibility,
+    setEditExpenseModalVisibility,
+    savedExpenseCallBack,
+}) {
+    const axiosPrivate = useAxiosPrivate();
+    const controller = new AbortController();
+    const expenseNameRef = useRef();
+    const errorRef = useRef();
+    const defaultExpense = {
+        expenseId: expense.id,
+        narration: expense.narration,
+        expenseTypeId: expense.expenseType.expenseTypeId,
+        paymentMethodId: expense.paymentMethod.paymentMethodId,
+        amount: `${expense.amount}`,
+    };
+
+    const [isMounted, setIsMounted] = useState(true);
+    const [allExpenseTypes, setAllExpenseTypes] = useState([]);
+    const [allPaymentMethods, setAllPaymentMethods] = useState([]);
+    const [isEditExpenseFormValidated, setIsEditExpenseFormValidated] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [editedExpense, setEditedExpense] = useState(defaultExpense);
+
+    useEffect(() => {
+        expenseNameRef.current.focus();
+    }, [expenseNameRef]);
+
+    const getExpenseTypes = async () => {
+        const expenseTypes = await ExpenseTypesService.getAllExpenseTypes(
+            axiosPrivate,
+            controller,
+            setErrorMessage,
+        );
+        const allExpenseTypes = expenseTypes.map((expenseType) => {
+            return { value: expenseType.id, label: expenseType.name };
+        });
+        isMounted && setAllExpenseTypes(allExpenseTypes);
+    };
+
+    const expenseTypesWithPlaceholder = (expenseTypes) => {
+        return [{ value: '', label: 'Select expense type...' }, ...expenseTypes];
+    };
+
+    const paymentMethods = async () => {
+        const paymentMethods = await PaymentMethodsService.getAllPaymentMethods(
+            axiosPrivate,
+            controller,
+            setErrorMessage,
+        );
+        const allPaymentMethods = paymentMethods.map((paymentMethod) => {
+            return { value: paymentMethod.id, label: paymentMethod.name };
+        });
+        isMounted && setAllPaymentMethods(allPaymentMethods);
+    };
+
+    const paymentMethodsWithPlaceholder = (paymentMethods) => {
+        return [{ value: '', label: 'Select payment method...' }, ...paymentMethods];
+    };
+
+    const handleEditExpense = async (event) => {
+        const editExpenseForm = event.currentTarget;
+
+        if (editExpenseForm.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            event.preventDefault();
+            setErrorMessage('');
+            setIsLoading(true);
+
+            await ExpensesService.editExpense(
+                editedExpense,
+                axiosPrivate,
+                controller,
+                setErrorMessage,
+            ).then(
+                (response) => {
+                    setEditedExpense(defaultExpense);
+                    setEditExpenseModalVisibility(!visibility);
+                    savedExpenseCallBack(response);
+                },
+                (error) => {
+                    if (!error?.response) {
+                        setErrorMessage('No server response');
+                    }
+
+                    setErrorMessage(error.message);
+                    errorRef.current?.focus();
+                },
+            );
+        }
+
+        setIsLoading(false);
+        setIsEditExpenseFormValidated(true);
+    };
+
+    useEffect(() => {
+        getExpenseTypes();
+
+        return () => {
+            setIsMounted(false);
+            controller.abort();
+        };
+    }, []);
+
+    useEffect(() => {
+        paymentMethods();
+
+        return () => {
+            setIsMounted(false);
+            controller.abort();
+        };
+    }, []);
+
+    return (
+        <CModal
+            backdrop="static"
+            alignment="center"
+            visible={visibility}
+            onClose={() => setEditExpenseModalVisibility(!visibility)}
+            aria-labelledby="StaticBackdropExampleLabel"
+        >
+            <CModalHeader>
+                <CModalTitle id="StaticBackdropExampleLabel">Edit Expense</CModalTitle>
+            </CModalHeader>
+            <CModalBody>
+                <CContainer>
+                    <CRow className="justify-content-center">
+                        <CCol md={12} lg={12} xl={12}>
+                            <CCard className="mx-2">
+                                <CCardBody className="p-4">
+                                    {errorMessage && (
+                                        <CFormText className="mb-3" style={{ color: 'red' }}>
+                                            An error occured while saving the expense type. Please
+                                            try again!
+                                        </CFormText>
+                                    )}
+                                    <CForm
+                                        className="needs-validation"
+                                        noValidate
+                                        validated={isEditExpenseFormValidated}
+                                        onSubmit={handleEditExpense}
+                                        id="editExpenseForm"
+                                    >
+                                        <CFormInput
+                                            className="mb-3"
+                                            placeholder="Enter name"
+                                            autoComplete="off"
+                                            id="name"
+                                            label="Name"
+                                            required
+                                            ref={expenseNameRef}
+                                            value={editedExpense.narration}
+                                            onChange={(e) => {
+                                                setEditedExpense((prev) => {
+                                                    return {
+                                                        ...prev,
+                                                        narration: e.target.value,
+                                                    };
+                                                });
+                                            }}
+                                        />
+                                        <CFormSelect
+                                            className="mb-3"
+                                            placeholder="Select expense type..."
+                                            autoComplete="off"
+                                            options={expenseTypesWithPlaceholder(allExpenseTypes)}
+                                            id="expenseType"
+                                            label="Type"
+                                            required
+                                            value={editedExpense.expenseTypeId}
+                                            onChange={(e) => {
+                                                setEditedExpense((prev) => {
+                                                    return {
+                                                        ...prev,
+                                                        expenseTypeId: e.target.value,
+                                                    };
+                                                });
+                                            }}
+                                        />
+                                        <CFormLabel>Amount</CFormLabel>
+                                        <CInputGroup className="mb-3">
+                                            <CInputGroupText>ZMW</CInputGroupText>
+                                            <CFormInputWithMask
+                                                mask={parseFloat}
+                                                placeholder="Amount"
+                                                autoComplete="off"
+                                                id="amount"
+                                                required
+                                                type="number"
+                                                value={editedExpense.amount}
+                                                onChange={(e) => {
+                                                    setEditedExpense((prev) => {
+                                                        return {
+                                                            ...prev,
+                                                            amount: e.target.value,
+                                                        };
+                                                    });
+                                                }}
+                                            />
+                                        </CInputGroup>
+                                        <CFormSelect
+                                            className="mb-3"
+                                            placeholder="Select payment method..."
+                                            autoComplete="off"
+                                            options={paymentMethodsWithPlaceholder(
+                                                allPaymentMethods,
+                                            )}
+                                            id="paymentMethod"
+                                            label="Payment Method"
+                                            required
+                                            value={editedExpense.paymentMethodId}
+                                            onChange={(e) => {
+                                                setEditedExpense((prev) => {
+                                                    return {
+                                                        ...prev,
+                                                        paymentMethodId: e.target.value,
+                                                    };
+                                                });
+                                            }}
+                                        />
+                                    </CForm>
+                                </CCardBody>
+                            </CCard>
+                        </CCol>
+                    </CRow>
+                </CContainer>
+            </CModalBody>
+            <CModalFooter>
+                <CButton color="secondary" onClick={() => setEditExpenseModalVisibility(false)}>
+                    Cancel
+                </CButton>
+                <CLoadingButton
+                    color="primary"
+                    form="editExpenseForm"
+                    loading={isLoading}
+                    type="submit"
+                >
+                    Save Expense
+                </CLoadingButton>
+            </CModalFooter>
+        </CModal>
+    );
+}
+
+EditExpenseForm.propTypes = {
+    expense: PropTypes.object.isRequired,
+    visibility: PropTypes.bool.isRequired,
+    setEditExpenseModalVisibility: PropTypes.func.isRequired,
+    savedExpenseCallBack: PropTypes.func.isRequired,
+};
---
+++ src/views/cashbook/expenses/ExpensesGrid.js
@@ -0,0 +1,179 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, useRef } from 'react';
+import { CButton, CCardBody, CSmartTable, CToaster } from '@coreui/react-pro';
+import useAxiosPrivate from 'src/hooks/useAxiosPrivate.js';
+import CIcon from '@coreui/icons-react';
+import NewExpenseForm from './NewExpenseForm';
+import { SuccessToast } from 'src/components/common/SuccessToast';
+import EditExpenseForm from './EditExpenseForm';
+import { EditButton } from 'src/components/common/EditButton';
+import { cilPlus } from '@coreui/icons';
+import ExpensesService from 'src/api/cashbook/expenses.service';
+
+export default function ExpensesGrid() {
+    const axiosPrivate = useAxiosPrivate();
+    const controller = new AbortController();
+
+    const [expenses, setExpenses] = useState([]);
+    const [createdExpense, setCreatedExpense] = useState({});
+    const [error, setError] = useState('');
+    const [isMounted, setIsMounted] = useState(true);
+    const [isVisibleEditExpenseModal, setIsVisibleEditExpenseModal] = useState(false);
+    const [isVisibleNewExpenseModal, setIsVisibleNewExpenseModal] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [savedExpense, setSavedExpense] = useState({});
+    const [selectedExpense, setSelectedExpense] = useState({});
+    const [toast, setToast] = useState(0);
+
+    const expenseActionSuccessToasterRef = useRef();
+
+    const columns = [
+        { key: 'narration', label: 'Name', _style: { width: '30%' } },
+        {
+            key: 'expenseType',
+            label: 'Expense Type',
+            _style: { width: '30%' },
+        },
+        {
+            key: 'amount',
+            label: 'Amount',
+            _style: { width: '20%' },
+        },
+        {
+            key: 'paymentMethod',
+            label: 'Pament Method',
+            _style: { width: '20%' },
+        },
+        {
+            key: 'show_details',
+            label: '',
+            _style: { width: '1%' },
+            filter: false,
+            sorter: false,
+        },
+    ];
+
+    const getExpenses = async () => {
+        const response = await ExpensesService.getAllExpenses(axiosPrivate, controller, setError);
+        isMounted && setExpenses(response);
+        setLoading(false);
+    };
+
+    const setCreatedExpenseAndRefreshExpenses = (newExpense) => {
+        setCreatedExpense(newExpense);
+        getExpenses();
+    };
+
+    const setSavedExpenseAndRefreshExpenses = (savedEditedExpense) => {
+        setSavedExpense(savedEditedExpense);
+        getExpenses();
+    };
+
+    const formatExpenses = (expensesList) => {
+        return expensesList.map((expense) => {
+            return {
+                id: expense.id,
+                narration: expense.narration,
+                expenseType: expense.expenseType.typeName,
+                amount: expense.amount,
+                paymentMethod: expense.paymentMethod.typeName,
+                createdBy: expense.createdBy,
+            };
+        });
+    };
+
+    // get expense types data from api
+    useEffect(() => {
+        getExpenses();
+
+        return () => {
+            setIsMounted(false);
+            controller.abort();
+        };
+    }, []);
+
+    useEffect(() => {
+        const expenseSuccessfullyCreatedToast = (
+            <SuccessToast message={'New expense has been created successfully'} />
+        );
+
+        if (createdExpense?.narration) {
+            setToast(expenseSuccessfullyCreatedToast);
+        }
+    }, [createdExpense]);
+
+    useEffect(() => {
+        const expenseSuccessfullyEditedToast = (
+            <SuccessToast message={'Expense has been updated successfully'} />
+        );
+
+        if (savedExpense?.narration) {
+            setToast(expenseSuccessfullyEditedToast);
+        }
+    }, [savedExpense]);
+
+    return (
+        <>
+            <CCardBody>
+                <CButton
+                    color="primary"
+                    className="mb-2"
+                    variant="outline"
+                    size="sm"
+                    onClick={() => setIsVisibleNewExpenseModal(!isVisibleNewExpenseModal)}
+                >
+                    <CIcon icon={cilPlus} title="Add New Expense" /> Add New Expense
+                </CButton>
+                <CSmartTable
+                    sorterValue={{ column: 'description', state: 'asc' }}
+                    items={formatExpenses(expenses)}
+                    columns={columns}
+                    itemsPerPage={10}
+                    columnFilter
+                    columnSorter
+                    tableFilter
+                    loading={loading}
+                    cleaner
+                    itemsPerPageSelect
+                    pagination
+                    noItemsLabel={
+                        error
+                            ? `Could not retrieve expenses due to ${error}. Please try again.`
+                            : 'No expenses found'
+                    }
+                    scopedColumns={{
+                        show_details: (item) => (
+                            <EditButton
+                                item={expenses.find((expense) => expense.id === item.id)}
+                                setSelectedItem={setSelectedExpense}
+                                isVisibleEditModal={isVisibleEditExpenseModal}
+                                setIsVisibleEditModal={setIsVisibleEditExpenseModal}
+                            />
+                        ),
+                    }}
+                    tableProps={{
+                        hover: true,
+                        responsive: true,
+                        striped: true,
+                    }}
+                />
+            </CCardBody>
+            {isVisibleNewExpenseModal && (
+                <NewExpenseForm
+                    visibility={isVisibleNewExpenseModal}
+                    setExpenseModalVisibility={setIsVisibleNewExpenseModal}
+                    createdExpenseCallBack={setCreatedExpenseAndRefreshExpenses}
+                />
+            )}
+            {isVisibleEditExpenseModal && (
+                <EditExpenseForm
+                    expense={selectedExpense}
+                    visibility={isVisibleEditExpenseModal}
+                    setEditExpenseModalVisibility={setIsVisibleEditExpenseModal}
+                    savedExpenseCallBack={setSavedExpenseAndRefreshExpenses}
+                />
+            )}
+            <CToaster ref={expenseActionSuccessToasterRef} push={toast} placement="bottom-end" />
+        </>
+    );
+}
